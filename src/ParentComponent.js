e1881741ac45f7fec6ffe6617c32d7c43d03a7a8
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-27 05:22:03 +0000

    first

---
+++ src/ParentComponent.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function ParentComponent() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSubmit = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div>
+      <h2>Parent Component</h2>
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <ChildComponent handleLoginSubmit={handleLoginSubmit} />
+      )}
+    </div>
+  );
+}
+
+function ChildComponent({ handleLoginSubmit }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleLoginSubmit();
+  };
+
+  return (
+    <div>
+       {/* <h3>Child Component - Login Form</h3> */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default ParentComponent;
